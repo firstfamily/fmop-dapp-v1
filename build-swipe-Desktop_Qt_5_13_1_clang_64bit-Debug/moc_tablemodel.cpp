b80dc3a6ba471609295a13270f46b1cac61cf480
陈朵拉 <conan8chan@yahoo.com>
2019-11-27 16:39:39 +0800

    update rp

---
+++ build-swipe-Desktop_Qt_5_13_1_clang_64bit-Debug/moc_tablemodel.cpp
@@ -0,0 +1,128 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'tablemodel.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.13.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../swipe/tablemodel.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'tablemodel.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.13.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_TableModel_t {
+    QByteArrayData data[7];
+    char stringdata0[64];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_TableModel_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_TableModel_t qt_meta_stringdata_TableModel = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "TableModel"
+QT_MOC_LITERAL(1, 11, 10), // "headerData"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 7), // "section"
+QT_MOC_LITERAL(4, 31, 15), // "Qt::Orientation"
+QT_MOC_LITERAL(5, 47, 11), // "orientation"
+QT_MOC_LITERAL(6, 59, 4) // "role"
+
+    },
+    "TableModel\0headerData\0\0section\0"
+    "Qt::Orientation\0orientation\0role"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_TableModel[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // methods: name, argc, parameters, tag, flags
+       1,    3,   24,    2, 0x02 /* Public */,
+       1,    2,   31,    2, 0x22 /* Public | MethodCloned */,
+
+ // methods: parameters
+    QMetaType::QVariant, QMetaType::Int, 0x80000000 | 4, QMetaType::Int,    3,    5,    6,
+    QMetaType::QVariant, QMetaType::Int, 0x80000000 | 4,    3,    5,
+
+       0        // eod
+};
+
+void TableModel::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<TableModel *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: { QVariant _r = _t->headerData((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< Qt::Orientation(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3])));
+            if (_a[0]) *reinterpret_cast< QVariant*>(_a[0]) = std::move(_r); }  break;
+        case 1: { QVariant _r = _t->headerData((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< Qt::Orientation(*)>(_a[2])));
+            if (_a[0]) *reinterpret_cast< QVariant*>(_a[0]) = std::move(_r); }  break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject TableModel::staticMetaObject = { {
+    &QAbstractTableModel::staticMetaObject,
+    qt_meta_stringdata_TableModel.data,
+    qt_meta_data_TableModel,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *TableModel::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *TableModel::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_TableModel.stringdata0))
+        return static_cast<void*>(this);
+    return QAbstractTableModel::qt_metacast(_clname);
+}
+
+int TableModel::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QAbstractTableModel::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
